13c7cb1593e5771a90dfa0825ebb365f16a7fd70
Gabriel Guerra <gfranklings@gmail.com>
2024-06-02 11:52:08 -0400

    chore: exposing a nix overlay for the cutefetch package

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -6,6 +6,8 @@
   cutefetch = pkgs.callPackage ./cutefetch.nix {};
 in {
   packages.default = cutefetch;
+  overlays.default = (_: _: {inherit cutefetch;});
+
   devShells.default = pkgs.mkShell {
     buildInputs = [cutefetch];
   };
